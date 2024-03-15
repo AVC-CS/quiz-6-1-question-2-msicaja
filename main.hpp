58835db76921987a641acd7922cd9fc4ed9d4366
msicaja <156959957+msicaja@users.noreply.github.com>
2024-03-15 22:44:19 +0000

    First Run

--- main.hpp
+++ main.hpp
@@ -1,3 +1,37 @@
 /***************************************************
  * Code your program here
  ***************************************************/
+ #include <iostream>
+ #include <fstream>
+ using namespace std;
+ int writeFile(string);
+ int readFile(string);
+
+ int writeFile(string fileName) {
+    ofstream ofs;
+
+    ofs.open(fileName);
+
+    if (!ofs) {
+        cout << "File Open Error\n";
+        exit(0);
+    }
+
+    int N, id, i;
+    string depart, name;
+    double salary;
+
+    cin >> N;
+    ofs << N << endl;
+
+    for (i = 0; i < N; ++i) {
+        cin >> id >> name >> depart >> salary;
+        ofs << id << " " << name << " " << depart << " " << salary;
+    }
+
+    return N;
+ }
+
+ int readFile(string fileName) {
+    return 23;
+ }
